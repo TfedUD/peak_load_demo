d7e65a40aa660f2f4844518575209c45f2701ccf
trevor <63176386+TfedUD@users.noreply.github.com>
2024-06-11 16:03:38 -0400

    feat: hover data

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ st.set_page_config(layout="wide",
                    page_title='Sensitivity Study',
                    page_icon='8100464.png')
 
-st.markdown('''For this study a PNNL prototype building was selected ''')
+
+st.markdown('''For this study a PNNL prototype building was selected to be subjected to a parametric study.  
+                ''')
 
 df = pd.read_csv('data.csv')
 
@@ -20,6 +22,7 @@ df = df.rename(columns={'in:Shade_Count':'Shade Count',
                         'out:Heating Energy Kwh': 'Heating Energy Kwh'})
 df['Is Shade Vertical'] = df['Is Shade Vertical'].astype(int)
 
+
 fig = px.parallel_coordinates(df, color='Cooling Energy Kwh', color_continuous_scale='Portland')
 st.plotly_chart(fig, use_container_width=True)
 
@@ -29,36 +32,37 @@ st.plotly_chart(fig, use_container_width=True)
 col1,col2,col3,col4 = st.columns(4)
 
 with col1:
-    fig1 = px.scatter(df, x='Shade Count', y='Cooling Energy Kwh')
+    fig1 = px.scatter(df, x='Shade Count', y='Cooling Energy Kwh', hover_data=['Shade Depth', 'Is Shade Vertical','Angle'])
     st.plotly_chart(fig1, use_container_width=True)
 
 with col2:
-    fig2 = px.scatter(df, x='Shade Depth', y='Cooling Energy Kwh')
+    fig2 = px.scatter(df, x='Shade Depth', y='Cooling Energy Kwh', hover_data=['Shade Count', 'Is Shade Vertical', 'Angle'])
     st.plotly_chart(fig2, use_container_width=True)
     
 with col3:
-    fig3 = px.scatter(df, x='Is Shade Vertical', y='Cooling Energy Kwh') 
+    fig3 = px.scatter(df, x='Is Shade Vertical', y='Cooling Energy Kwh', hover_data=['Shade Count', 'Shade Depth', 'Angle']) 
     st.plotly_chart(fig3, use_container_width=True)
 
 with col4:
-    fig4 = px.scatter(df, x='Angle', y='Cooling Energy Kwh')
+    fig4 = px.scatter(df, x='Angle', y='Cooling Energy Kwh', hover_data=['Shade Count', 'Shade Depth', 'Is Shade Vertical'])
     st.plotly_chart(fig4, use_container_width=True)
     
     
 col11,col12,col13,col14 = st.columns(4)
 
 with col11:
-    fig11 = px.scatter(df, x='Shade Count', y='Heating Energy Kwh', color_discrete_sequence=['red'])
+    fig11 = px.scatter(df, x='Shade Count', y='Heating Energy Kwh', color_discrete_sequence=['red'], hover_data=['Shade Depth', 'Is Shade Vertical','Angle'])
     st.plotly_chart(fig11, use_container_width=True)
     
 with col12:
-    fig12 = px.scatter(df, x='Shade Depth', y='Heating Energy Kwh', color_discrete_sequence=['red'])
+    fig12 = px.scatter(df, x='Shade Depth', y='Heating Energy Kwh', color_discrete_sequence=['red'], hover_data=['Shade Count', 'Is Shade Vertical', 'Angle'])
     st.plotly_chart(fig12, use_container_width=True)
     
 with col13:
-    fig13 = px.scatter(df, x='Is Shade Vertical', y='Heating Energy Kwh', color_discrete_sequence=['red'])
+    fig13 = px.scatter(df, x='Is Shade Vertical', y='Heating Energy Kwh', color_discrete_sequence=['red'], hover_data=['Shade Count', 'Shade Depth', 'Angle'])
     st.plotly_chart(fig13, use_container_width=True)
     
 with col14:
-    fig14 = px.scatter(df, x='Angle', y='Heating Energy Kwh', color_discrete_sequence=['red'])
+    fig14 = px.scatter(df, x='Angle', y='Heating Energy Kwh', color_discrete_sequence=['red'], hover_data=['Shade Count', 'Shade Depth', 'Is Shade Vertical'])
     st.plotly_chart(fig14, use_container_width=True)
+    
